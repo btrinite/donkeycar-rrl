186009c58eff8f4f90479a5750ead1c7b9a023b8
btrinite <btrinite@free.fr>
2023-01-03 19:00:09 +0100

    fix

--- donkeycar/templates/complete.py
+++ donkeycar/templates/complete.py
@@ -432,7 +432,7 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None,
         V.add(ObstacleDetector(cfg.OBSTACLE_MIN_SCORE,
                                cfg.OBSTACLE_SHOW_BOUNDING_BOX),
               inputs=['cam/image_array'],
-              outputs=[cam/image_array'])
+              outputs=['cam/image_array'])
 
     #
     # to give the car a boost when starting ai mode in a race.
