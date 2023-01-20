25be50c0eecc4469b972b316e52df184d2481000
btrinite <btrinite@free.fr>
2023-01-20 18:51:34 +0100

    Log with colors

--- donkeycar/utilities/logger.py
+++ donkeycar/utilities/logger.py
@@ -1,12 +1,35 @@
 import logging
 
+class CustomFormatter(logging.Formatter):
+
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    #format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
 def init_special_logger(namespace):
     _logger = logging.getLogger(namespace)
     _logger.setLevel(logging.DEBUG)
     sh = logging.StreamHandler()
     _logger.addHandler(sh)
-    formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
-    sh.setFormatter(formatter)
+    #formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+    sh.setFormatter(CustomFormatter())
     _logger.propagate = False
     return _logger
 
